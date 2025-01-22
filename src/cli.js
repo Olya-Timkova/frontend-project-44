0f85bd02f59e5fde4f0a6ac2f5f62a4c4e77f639
Olya-Timkova <o.timkova@bk.ru>
2025-01-22 22:35:42 +0300

    add directory src with file cli.js and file package-lock.json

---
+++ src/cli.js
@@ -0,0 +1,12 @@
+
+import readlineSync from 'readline-sync';
+
+export const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name?')
+ 
+//  Ожидаем ответа пользователя.
+export const  userName  = readlineSync.question( 'Hello, ' + name + '!') ; 
+// console.log ( ' Hello ' +  userName  + ' ! ' ) ;​​  
+
+
+ 
+
